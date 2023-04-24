146f2be727bf1e94afff507bd10db92c614f7bad
kameleon-ad <trinitrotoluene04@hotmail.com>
2023-04-24 19:33:13 +0000

    GLM-10-Chinese is downloadable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, AutoModelForSeq2SeqLM
 
 from absl import app
 
@@ -18,20 +18,21 @@ def test_chat_glm():
 
 
 def test_glm():
-    tokenizer = AutoTokenizer.from_pretrained(ChatGLM, trust_remote_code=True)
-    model = AutoModel.from_pretrained(ChatGLM, trust_remote_code=True).half().cuda()
+    tokenizer = AutoTokenizer.from_pretrained(GLM, trust_remote_code=True)
+    model = AutoModelForSeq2SeqLM.from_pretrained(GLM, trust_remote_code=True)
+    model = model.half().cuda()
 
     inputs = tokenizer("凯旋门位于意大利米兰市古城堡旁。1807年为纪念[MASK]而建，门高25米，顶上矗立两武士青铜古兵车铸像。",
                        return_tensors="pt")
-    # inputs = tokenizer.build_inputs_for_generation(inputs, max_gen_length=512)
-    # inputs = {key: value.cuda() for key, value in inputs.items()}
-    outputs = model.generate(inputs["input_ids"], max_length=512)
+    inputs = tokenizer.build_inputs_for_generation(inputs, max_gen_length=512)
+    inputs = {key: value.cuda() for key, value in inputs.items()}
+    outputs = model.generate(**inputs, max_length=512, eos_token_id=tokenizer.eop_token_id)
     print(tokenizer.decode(outputs[0].tolist()))
 
 
 def main(_):
-    # print("[INFO] ChatGLM")
-    # test_chat_glm()
+    print("[INFO] ChatGLM")
+    test_chat_glm()
     print("[INFO] GLM")
     test_glm()
     return 0
